c5a138e06db08d862ded200057f5c3e4be4c8c7b
LinpuLiu <71500435+LinpuLiu@users.noreply.github.com>
2020-10-20 14:03:26 +0800

    Added the my code C.py to change CD.py to call from C.py.

--- Lab-5/CD.py
+++ Lab-5/CD.py
@@ -1,7 +1,8 @@
 # SYSC3010 Lab 5 - CD.py
 #     The file to be jointly edited by Team Members C and D
 from D import *
+from C import * #line is added by Teammember C
 def function_CD():
     print ("CD")
-    
+    function_C()
     function_D()
