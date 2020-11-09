d49aef14b169fa6cc7a7c0b46797121421b6c083
Zoya Mushtaq <zoya.mushtaq@carleton.ca>
2020-11-08 19:15:29 -0500

    the proper prototype for the app

---
+++ APP_DEV/COVID19LabratoryMonitoringSystem/app/src/main/java/com/example/covid_19labratorymonitoringsystem/Lab1.java
@@ -0,0 +1,24 @@
+package com.example.covid_19labratorymonitoringsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Lab1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab1);
+    }
+
+
+
+  /*  public void get_pressure(View view) {
+        EditText myText = (EditText)findViewById(R.id.changepressure);
+        String text = "here put the text that you want";
+        myText.setText(text); //variable from point 2
+    }*/
+}
---
+++ APP_DEV/COVID19LabratoryMonitoringSystem/app/src/main/java/com/example/covid_19labratorymonitoringsystem/hq.java
@@ -0,0 +1,14 @@
+package com.example.covid_19labratorymonitoringsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hq extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hq);
+    }
+}
