b1feaf37bca68463dc56673f1d327de072efcd47
ifiokudoh <ifiokudoh@ymail.com>
2020-11-18 09:59:52 -0500

    Updated code for Second demo

--- RemotePatientLab.py
+++ RemotePatientLab.py
@@ -1,56 +1,15 @@
-# COVID-19 Lab Simulation
-# Remote Patient Lab
-# date: Nov 12 2020
-# name: Zoya Mushtaq
-# description: Remote Patient Lab set up
-class Patient:
+import sys
+sys.path.append('..')
+from Communication import Node
 
-    self.Name
-    self.Age
-    self.Gender
-    self.Temperature
+class RemotePatientLab(Node.Node):
+    def __init__(self, thingSpeak_url, readKey, writeKey):
+        super(RemotePatientLab, self).__init__(thingSpeak_url, readKey, writeKey,"remotePatientLab")
+        self.ReadList = []
 
-    #define setTemperature function
-    def setTemperature(self, Integer):
-        this.temperature=Temperature
+    # def FormatData(self,data):
 
-
-    #define getTemperature function
-    def getTemperature(self):
-        Temperature = sense.get_temperature()
-
-    #define setName function
-    def setName(self,name):
-        this.name = name
-
-    #define getName function
-    def getName(self):
-        return self.Name
-
-    #define setAge function
-    def setAge(self,Age):
-        self.age = Age
-
-    #define getAge function
-    def getAge():
-        return self.Age 
-
-    #define setGender function
-    def setGender(self, gender):
-        self.gender=gender
-
-    #define getGender function
-    def getGender(self):
-        return self.Gender
-
-class RPL:
-
-    self.Name
-    self.Age
-    self.tempThreshold
-
-
-
-
-
-
+    def process_data(self):
+        # print("remotePatientLab READ: ", self.read_data_pointer[0], " from: ", self.read_sender_pointer[0])
+        a =  self.read_data_pointer[0]
+        self.ReadList.append(int(a.encode("ascii"))) # encode read unicode string to ascii and convert to integer
